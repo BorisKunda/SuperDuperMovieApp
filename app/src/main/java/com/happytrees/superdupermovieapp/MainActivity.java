3f0aecf3d653cdaeca63960d1385514ba4b8f356
limudimwork <boriskunda@gmail.com>
2018-08-25 16:15:37 +0300

    search icon

--- app/src/main/java/com/happytrees/superdupermovieapp/MainActivity.java
+++ app/src/main/java/com/happytrees/superdupermovieapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.happytrees.superdupermovieapp;
 
 import android.app.FragmentManager;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //BUGS:
-    boolean splashed = false;
+
+    private boolean splashed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
         if (splashed != true) {//if splashed is true then activity already had called splash fragment once
             splashFrag();
         }
@@ -30,6 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "search clicked",Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+        return true;
+    }
 
     //adds splash fragment
     void splashFrag() {
